11c5270046558c2cdc00ba551aecad5abe2d2524
Dominic Sonntag <dominic.sonntag@sonntag.it>
2017-02-20 22:53:59 +0100

    first commit

---
+++ lib/SMS/API/CMTelecom.pm
@@ -0,0 +1,65 @@
+package SMS::API::CMTelecom;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+SMS::API::CMTelecom - SMS API for cmtelecom.com
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+    use SMS::API::CMTelecom;
+
+
+=head1 METHODS
+
+=head2 new
+
+=cut
+
+sub new {
+
+
+}
+
+=head2 send
+
+=cut
+
+sub send {
+
+}
+
+=head1 AUTHOR
+
+Dominic Sonntag, C<< <dominic at s5g.de> >>
+
+=head1 BUGS AND SUPPORT
+
+Please report any bugs or feature requests on Github: https://github.com/sonntagd/SMS-API-CMTelecom/issues
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 Dominic Sonntag.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+=cut
+
+1; # End of SMS::API::CMTelecom
